b7ca7b52aced8366350b7e741b79829a3ca6f7ff
Shubham Aggarwal <shubham.kittu20@gmail.com>
2024-07-03 16:43:12 +0530

    amex updates

---
+++ AMEX_Contest_2024/round_2/amex_campus_challenge_eval_script.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import sys
+
+
+### Instructions for unstop team : 
+'''
+You can use this code to measure Round2 performance of submissions by participants.
+### Datasets required:
+This script takes in 2 files as follows:
+
+primary_submission.csv -> This contains the match_id, dataset_type, win_pred_team_id, win_pred_score, train_algorithm, is_ensemble, train_hps_trees, train_hps_depth, train_hps_lr, *top 10 feature values. This is file submitted by participant.
+dep_var.csv    -> This contains match_id, dataset_type, win_team_id. This is private file available with Unstop team.
+
+Please ensure that the predicted_score column does not have any null columns and the column names are exactly matching as above.
+Please ensure that all these files are stored as ',' separated csv files.
+
+### How to use:
+To use this, first open the command line terminal, and call evaluation code script by passing the locations of submission and actual files respectively.
+Sample example of using commandline for running the script: 
+
+python Evaluation_Code.py path_to_primary_submission_file path_to_DepVar_file
+'''
+
+def checkDataType1(df):
+    assert (df['match id'].isna().sum() == 0), 'match id should not have NaNs'
+    assert (df['match id'].dtype == 'int64'), ('match id is not int64 type')
+    assert df['win_pred_team_id'].isna().sum() == 0, 'win_pred_team_id should not have NaNs'
+    assert df['win_pred_team_id'].dtype == 'int64', ('win_pred_team_id is not int64 type')
+    assert df['win_pred_score'].isna().sum() == 0, 'win_pred_score should not have NaNs'
+    assert df['win_pred_score'].dtype == 'float64', ('win_pred_score is not float64 type')
+    assert df['train_algorithm'].isna().sum() == 0, 'train_algorithm should not have NaNs'
+    assert df['train_algorithm'].dtype == 'object', ('train_algorithm is not object type')
+    assert df['is_ensemble'].isna().sum() == 0, 'is_ensemble should not have NaNs'
+    assert df['is_ensemble'].dtype == 'object', ('is_ensemble is not object type')
+    assert df['train_hps_trees'].isna().sum() == 0, 'train_hps_trees should not have NaNs'
+    assert df['train_hps_depth'].isna().sum() == 0, 'train_hps_depth should not have NaNs'
+    assert df['train_hps_lr'].isna().sum() == 0, 'train_hps_lr should not have NaNs'
+    return None
+
+def getAccuracy(df):
+  return round(df[df['winner_id']==df['win_pred_team_id']].shape[0]*100/df.shape[0],4)
+
+
+# sys.argv takes in the arguments that you pass from terminal. First argument is the location of Evaluation code file, Second argument is the location of primary submission file, Third argument is the location of secondary submission file and Fourth argument is the location of Dependent Variable file
+if len(sys.argv) != 3:
+  sys.exit("Please pass three files only as mentioned in the Instructions.")
+
+# Location of submission file.
+input1_address = sys.argv[1] 
+df_input1 = pd.read_csv(input1_address,sep=",",header=0)
+
+### For Unstop Team : Location of Dependent Variable files for Round1 or Round2. This is the private file available with unstop team. These files are comma separated
+round_eval = sys.argv[2]
+df_round = pd.read_csv(round_eval,sep=",",header=0)
+
+assert set(['match id', 'dataset_type','win_pred_team_id', 'win_pred_score', 'train_algorithm', 'is_ensemble', 'train_hps_trees','train_hps_depth','train_hps_lr']).issubset(set(df_input1.columns.tolist())), 'Required columns not present in primary submission file'
+assert set(['indep_feat_id1','indep_feat_id2','indep_feat_id3','indep_feat_id4','indep_feat_id5','indep_feat_id6','indep_feat_id7','indep_feat_id8','indep_feat_id9','indep_feat_id10']).issubset(set(df_input1.columns.tolist())), 'Required indepedent feature columns not present in primary submission file'
+
+checkDataType1(df_input1)
+
+assert df_input1.shape[0] == df_input1.drop_duplicates('match id').shape[0], 'Input file should be unique on match id'
+assert df_input1.shape[0] == 207, f'Input file size number of rows incorrect. Expected rowsize 207 not equal to uploaded data rowsize {df_input1.shape[0]}'
+assert df_input1.shape[1] == 19, 'Input file number of columns not correct. '
+assert (df_input1.win_pred_score.min() >= 0) & (df_input1.win_pred_score.max() <= 1), 'Win prediction score should be in range [0,1]'
+assert df_input1['train_algorithm'].nunique() == 1, 'only one algorithm can be used for all data'
+assert (len(df_input1['is_ensemble'].unique().tolist())==1)&((df_input1['is_ensemble'].unique().tolist()[0] == 'yes')|(df_input1['is_ensemble'].unique().tolist()[0] == 'no')), 'is_ensemble can take only \'yes\' or \'no\''
+assert df_input1.apply(lambda x: 0 if ( len(str(x['train_algorithm']).split(';')) == len(str(x['train_hps_trees']).split(';')) )&\
+  ( len(str(x['train_algorithm']).split(';')) == len(str(x['train_hps_depth']).split(';')) )&( len(str(x['train_algorithm']).split(';')) == len(str(x['train_hps_lr']).split(';')) ) else 1, axis=1).max() == 0, 'number of fields in algorithm & hyper-parameters column should be same.'
+
+shape_before_join = df_round.shape[0]
+
+r2_size = df_input1[df_input1['dataset_type']=='r2'].shape[0]
+assert (r2_size == df_round.shape[0]), f'R2 data size in input file is incorrect. Expected rowsize 207 not equal to r2 dataset_type present {r2_size}'
+
+# merging predicted file and dependent variable file
+eval_data = pd.merge(df_round,df_input1[df_input1['dataset_type']=='r2'], on=['match id'], how='inner').drop_duplicates()
+assert (eval_data.shape[0] == df_round.shape[0]), 'match ids in submission template does not match eval data'
+
+print('All checks passed...')
+print('Accuracy: ', round(getAccuracy(eval_data),2))
